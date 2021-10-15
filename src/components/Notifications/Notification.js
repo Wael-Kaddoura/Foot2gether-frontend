ef65312f4f179330dcf3fdfbf38c43556d93352a
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-15 22:07:45 +0300

    Setting Firebase Notification Token Generation

---
+++ src/components/Notifications/Notification.js
@@ -0,0 +1,22 @@
+import {useEffect} from "react";
+import firebase from "../../server/firebase";
+
+function Notification() {
+  useEffect(() => {
+    const msg = firebase.messaging();
+    msg
+      .requestPermission()
+      .then(() => {
+        return msg.getToken();
+      })
+      .then((data) => {
+        console.log("token", data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+  return <div></div>;
+}
+
+export default Notification;
