4223ab402227ea0ace66f7a2c05d9835c819f1b6
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-17 14:19:14 +0300

    Added Main Content to NavBar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,23 @@
+import MainNavBar from "../components/NavBar/MainNavBar";
+
 function Home() {
-  return <div>Hello</div>;
+  const NavBarContent = (
+    <div className="row align-items-center">
+      <div className="col-lg-5 ml-auto">
+        <h1 className="text-white text-center">Welcome to Foot2gether</h1>
+        <p className="text-center">
+          Enjoy watching Football Matches with Fans from around the Globe!
+        </p>
+        <p className="text-center">
+          <a href="matches.html" className="btn btn-primary py-3 px-4 mr-3">
+            View Matches
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+
+  return <MainNavBar NavBarContent={NavBarContent} />;
 }
 
 export default Home;
