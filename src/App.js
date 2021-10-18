d96c9485774d10c53a86ecb713e869bac865d70d
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-18 10:58:37 +0300

    Setting up the Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Notification from "./components/Notifications/Notification";
-import Room from "./pages/VideoRoom";
+import Home from "./pages/Home";
+import Matches from "./pages/Matches";
+import Blog from "./pages/Blog";
+import Standings from "./pages/Standings";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
-          <div> Welcome to Foot2gether </div>
+        <Route exact path="/home">
+          <Home />
         </Route>
-        <Route exact path="/room">
-          <Room />
+        <Route exact path="/matches">
+          <Matches />
         </Route>
-        <Route exact path="/not">
-          <Notification />
+        <Route exact path="/blog">
+          <Blog />
+        </Route>
+        <Route exact path="/standings">
+          <Standings />
         </Route>
       </Switch>
     </Router>
