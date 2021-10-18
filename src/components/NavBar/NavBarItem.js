366b12f532b30f6e33a4ccbf91c840f11d058a1e
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-18 10:53:07 +0300

    Added NavBarItem

---
+++ src/components/NavBar/NavBarItem.js
@@ -0,0 +1,26 @@
+import "../../css/bootstrap/bootstrap.css";
+import "../../css/owl.carousel.min.css";
+import "../../css/owl.theme.default.min.css";
+import "../../css/jquery-ui.css";
+import "../../css/jquery.fancybox.min.css";
+import "../../css/bootstrap-datepicker.css";
+import "../../css/aos.css";
+import "../../css/style.css";
+import { ListItem, ListItemText } from "@mui/material";
+import { Link } from "react-router-dom";
+
+function MainNavBar({ name, color }) {
+  return (
+    <Link
+      to={"/" + name}
+      className="nav-link"
+      style={{ textDecoration: "none", color: color }}
+    >
+      <ListItem button>
+        <ListItemText primary={name} />
+      </ListItem>
+    </Link>
+  );
+}
+
+export default MainNavBar;
