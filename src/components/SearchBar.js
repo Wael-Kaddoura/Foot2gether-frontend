5f5543bfe790b28d85461c529c37d95c371478aa
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-19 11:23:26 +0300

    Modified Color of Search Bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,31 +3,27 @@ import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
-import DirectionsIcon from "@mui/icons-material/Directions";
+import Grid from "@mui/material/Grid";
 
 export default function SearchBar() {
   return (
-    <Paper
-      component="form"
-      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
-    >
-      <IconButton sx={{ p: "10px" }} aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search Google Maps"
-        inputProps={{ "aria-label": "search google maps" }}
-      />
-      <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton color="primary" sx={{ p: "10px" }} aria-label="directions">
-        <DirectionsIcon />
-      </IconButton>
-    </Paper>
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+        style={{ backgroundColor: "rgba(255, 255, 255, 0.45)" }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Room by ID"
+          inputProps={{ "aria-label": "Search Room by ID" }}
+        />
+        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+      </Paper>
+    </Grid>
   );
 }
