e82049bd137a8b16d79301411a29323507d4f884
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-19 11:50:07 +0300

    Added NavBar Content

--- src/pages/Matches.js
+++ src/pages/Matches.js
@@ -1,16 +1,33 @@
 import MainNavBar from "../components/NavBar/MainNavBar";
-import LiveMatchCard from "../components/Matches/MatchCards/LiveMatchCard";
-import UpcomingMatchCard from "../components/Matches/MatchCards/UpcomingMatchCard";
-import FinishedMatchCard from "../components/Matches/MatchCards/FinishedMatchCard";
+import MatchesTab from "../components/Matches/MatchesTab";
+import SearchBar from "../components/SearchBar";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  pageTitle: {
+    fontSize: "50px !important",
+    fontWeight: 700,
+  },
+});
 
 function Matches() {
+  const classes = useStyles();
+
+  const NavBarContent = (
+    <div className="row align-items-center">
+      <div className="col-lg-5 mx-auto text-center">
+        <h1 className={classes.pageTitle}>Matches</h1>
+        <p className={classes.pageSubTitle}>
+          Choose any Match to enjoy watching the game with other fans!
+        </p>
+        <SearchBar />
+      </div>
+    </div>
+  );
   return (
     <div>
-      <MainNavBar currentPageName="Matches" />
-      <LiveMatchCard />
-      <UpcomingMatchCard />
-      <FinishedMatchCard />
-      <FinishedMatchCard />
+      <MainNavBar currentPageName="Matches" NavBarContent={NavBarContent} />
+      <MatchesTab />
     </div>
   );
 }
