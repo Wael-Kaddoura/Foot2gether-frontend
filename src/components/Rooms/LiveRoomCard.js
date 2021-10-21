ff6792f29edbf41b54d4b5018d1beae8ae622531
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-21 19:10:11 +0300

    Added Live Rooms Cards

---
+++ src/components/Rooms/LiveRoomCard.js
@@ -0,0 +1,101 @@
+import { Card, Grid, Typography, Button } from "@mui/material";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    backgroundColor: "rgba(255, 255, 255, 0.05)",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  roomName: {
+    color: "#fff",
+    fontSize: 22,
+    fontWeight: 700,
+    textAlign: "center",
+  },
+  joinBtn: {
+    color: "#fff",
+    backgroundColor: "#ee1e46",
+    borderColor: "#ee1e46",
+    fontSize: 16,
+    fontWeight: 700,
+  },
+  roomDetailsTitle: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: 300,
+    textAlign: "center",
+  },
+  roomDetails: {
+    color: "#fff",
+    fontSize: 17,
+    fontWeight: 300,
+    textAlign: "center",
+  },
+}));
+
+function LiveRoomCard(props) {
+  const {} = props;
+  const classes = useStyles();
+
+  return (
+    <Grid item xs={12} sx={{ mb: 5 }}>
+      <Card
+        className={classes.card}
+        sx={{ maxWidth: 1140, minHeight: 176, maxHeight: 176, mx: 2 }}
+      >
+        <Grid
+          sx={{ minHeight: 176 }}
+          container
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12}>
+            <Grid container justifyContent="space-between" alignItems="center">
+              <Grid item xs={4} md={2}>
+                <Typography className={classes.roomName}>Room01</Typography>
+              </Grid>
+
+              <Grid item xs={3} md={1}>
+                <Button
+                  className={classes.joinBtn}
+                  variant="outlined"
+                  color="error"
+                >
+                  Join
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Grid container justifyContent="center" alignItems="center">
+              <Grid item xs={4}>
+                <Typography className={classes.roomDetailsTitle}>
+                  Room ID:
+                </Typography>
+                <Typography className={classes.roomDetails}>211</Typography>
+              </Grid>
+
+              <Grid item xs={4}>
+                <Typography className={classes.roomDetailsTitle}>
+                  Created By:
+                </Typography>
+                <Typography className={classes.roomDetails}>Admin01</Typography>
+              </Grid>
+
+              <Grid item xs={4}>
+                <Typography className={classes.roomDetailsTitle}>
+                  Current:
+                </Typography>
+                <Typography className={classes.roomDetails}>25</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Card>
+    </Grid>
+  );
+}
+
+export default LiveRoomCard;
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,5 +1,82 @@
+import { makeStyles } from "@mui/styles";
+import { Grid, Button } from "@mui/material";
+import MainNavBar from "../components/NavBar/MainNavBar";
+import SearchBar from "../components/SearchBar";
+import LiveRoomCard from "../components/Rooms/LiveRoomCard";
+import CircleIcon from "@mui/icons-material/Circle";
+
+const useStyles = makeStyles({
+  pageTitle: {
+    fontSize: "50px !important",
+    fontWeight: 700,
+  },
+  roomsContainer: {
+    maxWidth: 1140,
+  },
+  bodyTitle: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: 500,
+  },
+  roomContent: {
+    minWidth: "100%",
+  },
+});
+
 function Rooms() {
-  return <div></div>;
+  const classes = useStyles();
+
+  const NavBarContent = (
+    <div className="row align-items-center">
+      <div className="col-lg-5 mx-auto text-center">
+        <h1 className={classes.pageTitle}>Rooms</h1>
+        <p className={classes.pageSubTitle}>
+          There are currently 23 Live Rooms!
+        </p>
+        <SearchBar />
+      </div>
+    </div>
+  );
+
+  return (
+    <div>
+      <MainNavBar currentPageName="Rooms" NavBarContent={NavBarContent} />
+
+      <Grid
+        container
+        className={classes.roomContent}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} className={classes.roomsContainer}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            sx={{ mt: 5 }}
+          >
+            <Grid item xs={12} sx={{ mb: 5 }}>
+              <Grid
+                container
+                direction="row"
+                alignItems="center"
+                className={classes.bodyTitle}
+              >
+                <CircleIcon style={{ fill: "#ee1e46" }} sx={{ mr: 1 }} />
+                Live Rooms
+              </Grid>
+            </Grid>
+
+            <LiveRoomCard />
+            <LiveRoomCard />
+            <LiveRoomCard />
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Rooms;
