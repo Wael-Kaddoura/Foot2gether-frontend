67ae50406105e1acf2b3d860eb5f4f4e593411bc
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-11-02 17:41:37 +0200

    Added useAxiosFetch

--- src/pages/AdminPanel/AdminMainPage.js
+++ src/pages/AdminPanel/AdminMainPage.js
@@ -13,54 +13,63 @@ function AdminMainPage() {
     history.push("/home");
   }
 
-  const {
-    data: cardsCount,
-    fetchError,
-    isPending,
-  } = useAxiosFetch("http://localhost:8000/admin/cards_count");
+  const { data: allMatchesCount, isPending: isAllMatchesPending } =
+    useAxiosFetch("http://localhost:8000/admin/match/all");
+
+  const { data: todaysMatchesCount, isPending: isTodaysMatchesPending } =
+    useAxiosFetch("http://localhost:8000/admin/match/today");
+
+  const { data: todaysRoomsCount, isPending: isTodaysRoomsPending } =
+    useAxiosFetch("http://localhost:8000/admin/room/today");
 
   return (
     <div>
       <AdminNavBar pageTitle="Foot2gether Admin Panel">
         <Backdrop
           sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-          open={isPending}
+          open={
+            isAllMatchesPending ||
+            isTodaysMatchesPending ||
+            isTodaysRoomsPending
+          }
         >
           <CircularProgress color="inherit" />
         </Backdrop>
 
-        {!isPending && (
-          <Grid
-            item
-            xs={12}
-            container
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            style={{ minHeight: "100vh" }}
-          >
-            <AdminDashboardCard
-              cardTitle="Total Matches"
-              cardIconName="SportsSoccerIcon"
-              cardCount={cardsCount.total_matches_count}
-              pageLink="all_matches"
-            />
+        {!isAllMatchesPending &&
+          !isTodaysMatchesPending &&
+          !isTodaysRoomsPending && (
+            <Grid
+              item
+              xs={12}
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              style={{ minHeight: "100vh" }}
+            >
+              <AdminDashboardCard
+                cardTitle="Total Matches"
+                cardIconName="SportsSoccerIcon"
+                cardCount={allMatchesCount.length}
+                pageLink="all_matches"
+              />
 
-            <AdminDashboardCard
-              cardTitle="Today's Matches"
-              cardIconName="SportsIcon"
-              cardCount={cardsCount.todays_matches_count}
-              pageLink="todays_matches"
-            />
+              <AdminDashboardCard
+                cardTitle="Today's Matches"
+                cardIconName="SportsIcon"
+                cardCount={todaysMatchesCount.length}
+                pageLink="todays_matches"
+              />
 
-            <AdminDashboardCard
-              cardTitle="Today's Rooms"
-              cardIconName="PersonalVideoIcon"
-              cardCount={cardsCount.todays_rooms_count}
-              pageLink="todays_rooms"
-            />
-          </Grid>
-        )}
+              <AdminDashboardCard
+                cardTitle="Today's Rooms"
+                cardIconName="PersonalVideoIcon"
+                cardCount={todaysRoomsCount.length}
+                pageLink="todays_rooms"
+              />
+            </Grid>
+          )}
       </AdminNavBar>
     </div>
   );
