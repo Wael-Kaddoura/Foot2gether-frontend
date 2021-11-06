d8316eeb35d83a0a9333e4ba5e0d474d9ba8e17d
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-11-06 15:36:40 +0200

    Modified Blogs Alignment

--- src/components/Blogs/BlogCard.js
+++ src/components/Blogs/BlogCard.js
@@ -1,11 +1,23 @@
-import { Grid, Box, Typography } from "@mui/material";
+import {
+  Grid,
+  Box,
+  Typography,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Button,
+} from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
+  blogCard: {
+    backgroundColor: "transparent !important",
+    border: "none !important",
+    boxShadow: "none !important",
+  },
   blogImg: {
-    height: "auto",
-    width: "100%",
     borderRadius: "4%",
   },
   blogInfo: {
@@ -39,9 +51,47 @@ function BlogCard(props) {
   const classes = useStyles();
 
   return (
-    <Grid item xs={12} md={4} lg={3} sx={{ mb: 5, mr: 1 }}>
+    <Grid
+      item
+      xs={12}
+      md={4}
+      lg={3}
+      container
+      justifyContent="center"
+      sx={{ mb: 5, mr: 1 }}
+    >
       <Link to={"/blog_view?id=" + blogID}>
-        <img className={classes.blogImg} src={blogImg} alt="blogImage" />
+        <Card sx={{ maxWidth: 345 }} className={classes.blogCard}>
+          <CardMedia
+            component="img"
+            height="430"
+            image={blogImg}
+            alt="blogImage"
+            className={classes.blogImg}
+          />
+
+          <CardContent>
+            <Typography className={classes.blogDate}>
+              {`${new Date(blogDate)}`.substring(0, 16)}
+            </Typography>
+
+            <Typography className={classes.blogTitle} sx={{ mb: 2 }}>
+              {blogTitle}
+            </Typography>
+
+            <Typography className={classes.blogBody}>
+              {blogBody.substring(0, 115) + "..."}
+            </Typography>
+          </CardContent>
+
+          <CardActions>
+            <Typography className={classes.blogReadMore} sx={{ mt: 2, ml: 1 }}>
+              Read More
+            </Typography>
+          </CardActions>
+        </Card>
+
+        {/* <img className={classes.blogImg} src={blogImg} alt="blogImage" />
 
         <Box className={classes.blogInfo} sx={{ mt: 3 }}>
           <Typography className={classes.blogDate}>
@@ -59,7 +109,7 @@ function BlogCard(props) {
           <Typography className={classes.blogReadMore} sx={{ mt: 2 }}>
             Read More
           </Typography>
-        </Box>
+        </Box> */}
       </Link>
     </Grid>
   );
--- src/pages/UserPages/Blogs.js
+++ src/pages/UserPages/Blogs.js
@@ -12,9 +12,13 @@ import BackdropComponent from "../../components/BackdropComponent";
 import Footer from "../../components/Footer";
 
 const useStyles = makeStyles({
+  blogContent: {
+    minWidth: "100%",
+  },
   blogsContainer: {
-    paddingRight: "5em",
-    paddingLeft: "5em",
+    maxWidth: "1400px !important",
+    marginLeft: "auto !important",
+    marginRight: "auto !important",
   },
   createBlogFAB: {
     margin: 0,
@@ -55,25 +59,33 @@ function Blog() {
       {!isPending && (
         <Grid
           id="allBlogs"
-          className={classes.blogsContainer}
           container
+          className={classes.blogContent}
           direction="row"
-          justifyContent="space-around"
-          alignItems="flex-end"
-          sx={{ pt: 10 }}
-          style={{ backgroundColor: "#1a1e25" }}
+          justifyContent="center"
+          style={{ backgroundColor: "#1a1e25 " }}
         >
-          {blogsData.map((blog) => (
-            <BlogCard
-              key={blog.id}
-              blogID={blog.id}
-              blogImg={blog.image}
-              blogTitle={blog.title}
-              blogBody={blog.body}
-              blogDate={blog.updatedAt}
-              blogAuthor={blog.author.username}
-            />
-          ))}
+          <Grid
+            className={classes.blogsContainer}
+            container
+            direction="row"
+            justifyContent="space-around"
+            alignItems="flex-end"
+            sx={{ pt: 10, mx: 2 }}
+            style={{ backgroundColor: "#1a1e25" }}
+          >
+            {blogsData.map((blog) => (
+              <BlogCard
+                key={blog.id}
+                blogID={blog.id}
+                blogImg={blog.image}
+                blogTitle={blog.title}
+                blogBody={blog.body}
+                blogDate={blog.updatedAt}
+                blogAuthor={blog.author.username}
+              />
+            ))}
+          </Grid>
         </Grid>
       )}
       <Link to="/create_blog">
